91cc738731bab66725685e1599436ef73c230df6
James Paterson <j@presstube.com>
2016-05-15 14:50:49 -0400

    tracing

---
+++ resources/boiled-spheres.js
@@ -0,0 +1,28 @@
+(function (lib, img, cjs, ss) {
+
+var p; // shortcut to reference prototypes
+
+// library properties:
+lib.properties = {
+	width: 800,
+	height: 800,
+	fps: 30,
+	color: "#FFFFFF",
+	manifest: []
+};
+
+
+
+// symbols:
+
+
+
+// stage content:
+(lib.boiledspheres = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = null;
+
+})(lib = lib||{}, images = images||{}, createjs = createjs||{}, ss = ss||{});
+var lib, images, createjs, ss;
